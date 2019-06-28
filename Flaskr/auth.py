456aeb7a4b136ae707829970de26decc4f1113e6
Antony <tonik32@gmail.com>
2019-06-28 13:44:06 +0300

    Require Authentication in Other Views

--- Flaskr/auth.py
+++ Flaskr/auth.py
@@ -77,3 +77,13 @@ def load_logged_in_user():
 		g.user = get_db().execute(
 			'SELECT * FROM user WHERE id = ?', (user_id)
 		).fetchone()
+
+def login_required(view):
+	@functools.wraps(view)
+	def wrapped_viw(**kwargs):
+		if g.user is None: 
+			return redirect(url_for(auth.login))
+		
+		return view(**kwargs)
+	
+	return wrapped_view
