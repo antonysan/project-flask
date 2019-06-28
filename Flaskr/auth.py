94b39ee6a491114d09851b69396093ee1a98c505
Antony <tonik32@gmail.com>
2019-06-28 14:53:07 +0300

    Добавляет кавычки

--- Flaskr/auth.py
+++ Flaskr/auth.py
@@ -34,7 +34,7 @@ def register():
 				(username, generate_password_hash(password))
 			)
 			db.commit()
-			return redirect(url_for(auth.login))
+			return redirect(url_for('auth.login'))
 		
 
 		flash(error)
