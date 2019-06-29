fb5196978df5b04f31b8e19f6dff1757ebe58c19
Antony <tonik32@gmail.com>
2019-06-29 14:42:46 +0300

    Добавляет функции создание и редактирование

--- Flaskr/auth.py
+++ Flaskr/auth.py
@@ -61,7 +61,7 @@ def login():
 
 		if error is None: 
 			session.clear()
-			session[user_id] = user['id']
+			session['user_id'] = user['id']
 			return redirect(url_for('index'))
 
 		flash(error)
@@ -75,15 +75,21 @@ def load_logged_in_user():
 		g.user = None
 	else: 
 		g.user = get_db().execute(
-			'SELECT * FROM user WHERE id = ?', (user_id)
+			'SELECT * FROM user WHERE id = ?', (user_id,)
 		).fetchone()
 
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
 def login_required(view):
-	@functools.wraps(view)
-	def wrapped_view(**kwargs):
-		if g.user is None: 
-			return redirect(url_for('auth.login'))
-		
-		return view(**kwargs)
-	
-	return wrapped_view
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
